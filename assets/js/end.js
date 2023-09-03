478bd4cf8fec63d423bcee5705f0bd6df1cc5595
Chris-Tollan <>
2023-09-03 21:09:09 +0000

    add functions to save top 5 score to local storage

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,9 +1,13 @@
 // update score on end page//
 // make add to leadboard button diabled unless username typed //
+// log top scores to local storage and store top5 //
 let username = document.getElementById('clan');
 let addToLeaderboardButton = document.getElementById("add_to_leaderboard_button");
 let endScore = document.getElementById('endscore');
 let newestScore = localStorage.getItem('newestScore');
+
+let topScores = JSON.parse(localStorage.getItem('topScores')) || [];
+
 endScore.innerText = newestScore;
 
 username.addEventListener('keyup', () => {
@@ -11,6 +15,18 @@ username.addEventListener('keyup', () => {
 });
 
 addToLeaderboard = e => {
-    console.log("added to leaderboard");
+
     e.preventDefault();
+
+    let score = {
+        score: newestScore,
+        name: username.value
+    };
+    topScores.push(score);
+    topScores.sort((a, b) => {
+        return b.score - a.score;
+    });
+    topScores.splice(5);
+
+    localStorage.setItem('topScores', JSON.stringify(topScores));
 };
